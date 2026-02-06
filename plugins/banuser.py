06b8ad5217d811a28f07e903edd4f54b6011ce1d
Theprofesser7694 <Yogendraswami15@gmail.com>
2026-02-06 13:06:44 +0530

    Update banuser.py

--- plugins/banuser.py
+++ plugins/banuser.py
@@ -23,7 +23,7 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, UserNotParticipant
 from bot import Bot
 from config import *
-from helper_func import *
+from .helper_func import *
 from database.database import *
 
 
